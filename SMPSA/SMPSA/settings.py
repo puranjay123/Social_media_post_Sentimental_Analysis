ed9edb8a12cd6d7427fa88c56d0db3dd2d26fddc
RajeswarSharma <rjqwer47@gmail.com>
2021-09-01 13:21:40 +0530

    Initiated apps

--- SMPSA/SMPSA/settings.py
+++ SMPSA/SMPSA/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_page',
+    'facebook',
+    'instagram',
+    'youtube',
+    'twitter',
 ]
 
 MIDDLEWARE = [
